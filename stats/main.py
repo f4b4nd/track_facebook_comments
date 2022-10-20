b5d65f6c2074273c30ba015d934110368f6369e9
fabien <fabien.andrian@gmail.com>
2022-10-20 11:01:34 +0200

    stats ok

--- stats/main.py
+++ stats/main.py
@@ -1,5 +1,6 @@
 import re, sys
 import difflib
+import pandas as pd
 
 get_normalize_text = lambda x: re.sub("é", "e", re.sub("à", "a", re.sub("ü", "u", x.lower())))
 
@@ -37,25 +38,19 @@ get_cleaned_text = lambda x: re.sub("je|nous|vote|votons|pour|bravo|merci|tous|b
 
 get_match = lambda s, db: difflib.get_close_matches(s, db, 1, 0.1)[0] if len(difflib.get_close_matches(s, db, 1, 0.1)) > 0 else ""
 
-# matches = { comment: get_match(get_cleaned_text(get_normalize_text(comment)), database) for comment in comments}
-
 matches = {key : [] for key in database}
 for comment in comments:
-    db = get_match(get_cleaned_text(get_normalize_text(comment)), database)
-    if db != '':
-        matches[db].append(comment)
-print(matches)
-
-sys.exit()
-reversed_dict = {}
-for key, value in matches.items():
-    reversed_dict.setdefault(value, [])
-    reversed_dict[value].append(key)
-
-counts = {key: len(values) for key, values in reversed_dict.items()}
-
-print (reversed_dict)
-sys.exit()
+    found = get_match(get_cleaned_text(get_normalize_text(comment)), database)
+    if found != '':
+        matches[found].append(comment)
+#print(matches)
+#print(list(matches.keys()))
+counts = {key: len(values) for key, values in matches.items()}
+#print(counts)
+df = pd.DataFrame(counts, index=[0]).T
+#df.to_excel("../data/occurences.xlsx")   
+#print(df)
+#sys.exit()
 
 ########################
 
@@ -66,13 +61,18 @@ get_second_column = lambda x: re.sub("^.*;", "", x)
 
 comments = [get_second_column(data) for data in datas]
 
-matches = { comment: get_match(get_cleaned_text(get_normalize_text(comment)), database) for comment in comments }
+matches = {key : [] for key in database}
+for comment in comments:
+    found = get_match(get_cleaned_text(get_normalize_text(comment)), database)
+    if found != '':
+        matches[found].append(comment)
+print(matches)
 
-reversed_dict = {}
-for key, value in matches.items():
-    reversed_dict.setdefault(value, [])
-    reversed_dict[value].append(key)
+counts = {key: len(values) for key, values in matches.items()}
 
-counts = {key: len(values) for key, values in reversed_dict.items()}
+print(counts)
 
-print(reversed_dict)
+df = pd.DataFrame(counts, index=['Votes']).T
+df = df.sort_values(by=['Votes'], ascending=False)
+df.to_excel("../data/occurences.xlsx")   
+print(df)
